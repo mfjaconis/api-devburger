fd2e55dc47a15ac16f9dbd858bf050a1e5c3bcce
mfjaconis <matheusjaconis@gmail.com>
2024-05-28 16:40:36 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'cc29dd9da7dfdb507766fc32181a9b4f',
+  expiresIn: '5d',
+};
---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionsController';
+import ProductController from './app/controllers/ProductController';
+import CategoryController from './app/controllers/CategoryController';
+import authMiddleware from './app/middlewares/auth';
+import OrderController from './app/controllers/OrderController';
+
+const routes = new Router();
+
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionsController.store);
+
+routes.use(authMiddleware);
+routes.post('/products', upload.single('file'), ProductController.store);
+routes.get('/products', ProductController.index);
+routes.put('/products/:id', upload.single('file'), ProductController.update);
+
+routes.post('/categories', upload.single('file'), CategoryController.store);
+routes.get('/categories', CategoryController.index);
+routes.put('/categories/:id', upload.single('file'), CategoryController.update);
+
+routes.post('/orders', OrderController.store);
+routes.get('/orders', OrderController.index);
+routes.put('/orders/:id', OrderController.update);
+
+export default routes;
